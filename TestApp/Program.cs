e7ec0711e5c259c03b6de3d3bc1c2f8172a9d1ef
Urvatov <urvatov@gmail.com>
2024-02-18 17:17:30 +0300

    примитивная программа

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Введите текст" );
+            string? text = Console.ReadLine();
+
+
+            Dictionary<string, int> words = new Dictionary<string, int>();
+            char[] wordSeparations = { ' ', '.', ',', ';', ':', '!', '?', '-', '—', '\'', '\"' };
+
+            string[] splitText = text.Split(wordSeparations, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string word in splitText)
+            {
+                string clean = word.ToLower();
+
+                if (words.ContainsKey(clean)) { words[clean]++; }
+                else { words.Add(clean, 1); }
+            }
+
+            Console.WriteLine("Уникальные слова:");
+            foreach (var word in words)
+            {
+                Console.WriteLine($"{word.Key} : {word.Value}");
+            }
+
+
         }
     }
 }
